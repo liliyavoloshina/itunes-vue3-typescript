9ea9c0dd058691bba8762f0a78e697f5c16844eb
Liliya <liliyavoloshine@gmail.com>
2021-05-29 18:05:14 +0400

    starting styling

--- src/components/SearchMusic.vue
+++ src/components/SearchMusic.vue
@@ -1,10 +1,12 @@
 <template>
   <form @submit.prevent="search">
     <div class="search-inputs">
-      <div class="search-input"><label for="term">Search term:</label>
-        <input v-model="searchParam.term" id="term" type="text">
+      <div class="search-input">
+        <label for="term">Search term:</label>
+        <input v-model="searchParam.term" id="term" type="text" placeholder="Blink182...">
       </div>
-      <div class="search-input"><label for="entity">Search entity:</label>
+      <div class="search-input">
+        <label for="entity">Search entity:</label>
         <select v-model="searchParam.entity" id="entity">
           <option value="musicArtist" selected>The artist name</option>
           <option value="album">The album's title</option>
@@ -13,7 +15,7 @@
       </div>
     </div>
 
-    <button>Search!</button>
+    <button class="search-button">Search!</button>
   </form>
 
   <div v-if="loading">Loading...</div>
@@ -55,26 +57,57 @@ export default defineComponent({
 </script>
 
 <style scoped>
+:root {
+  --purple: rgb(131, 58, 180);
+  --gradient: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 50%,
+    rgba(252, 176, 69, 1) 100%
+  );
+}
+* {
+  box-sizing: border-box;
+}
 a {
   color: #42b983;
 }
 
 label {
-  margin: 0 0.5em;
   font-weight: bold;
+  margin-bottom: 0.5em;
 }
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+input,
+select {
+  padding: 0.5em;
+  width: 100%;
 }
+
 .search-inputs {
   display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-
+.search-input {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 20em;
+  margin-bottom: 1em;
+}
 .search-results {
   display: flex;
+}
+.search-button {
+  width: 10em;
+  padding: 0.5em;
+  cursor: pointer;
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 50%,
+    rgba(252, 176, 69, 1) 100%
+  );
 }
 </style>
