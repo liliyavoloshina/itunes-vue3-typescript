5b985d55a2f02cd38c3f94f48b299edbe74d7cd0
Liliya <liliyavoloshine@gmail.com>
2021-05-31 12:10:22 +0400

    build for production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/itunes-vue3-typescript/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
