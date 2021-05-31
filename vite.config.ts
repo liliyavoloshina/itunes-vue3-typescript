f41f6948e7efd15e957a0ba5a1223580eee165eb
Liliya <liliyavoloshine@gmail.com>
2021-05-31 13:51:16 +0400

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/',
-  // base: '/itunes-vue3-typescript/',
+  base: '/itunes-vue3-typescript/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
