727b9a76978fefe04d1d3497d8f752e8af0ffe0a
Liliya <liliyavoloshine@gmail.com>
2021-06-15 12:02:42 +0400

    fixed album view

--- src/components/SearchItem.vue
+++ src/components/SearchItem.vue
@@ -35,18 +35,17 @@
     <div class="info">
       <div class="artist-name"><span class="label">Artist name:</span> <a
           :href="item.artistViewUrl">{{item.artistName}}</a></div>
-      </div>
-      <div class="collection-name"><span class="label">Collection name:</span> <a
-          :href="item.collectionViewUrl">{{item.collectionName}}</a></div>
-      <div class="genre-name"><span class="label">Genre name:</span> {{item.primaryGenreName}}</div>
-      <div class="release-date"><span class="label">Release date:</span> {{new Date(item.releaseDate).toDateString()}}
-      </div>
     </div>
-    <div class="audio">
-      <audio controls preload="none">
-        <source :src="item.previewUrl" type="audio/mp4" />
-      </audio>
+    <div class="collection-name"><span class="label">Collection name:</span> <a
+        :href="item.collectionViewUrl">{{item.collectionName}}</a></div>
+    <div class="genre-name"><span class="label">Genre name:</span> {{item.primaryGenreName}}</div>
+    <div class="release-date"><span class="label">Release date:</span> {{new Date(item.releaseDate).toDateString()}}
     </div>
+  </div>
+  <div class="audio">
+    <audio controls preload="none">
+      <source :src="item.previewUrl" type="audio/mp4" />
+    </audio>
   </div>
 </template>
 
